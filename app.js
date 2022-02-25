19a674c57c1a018f8b1b4a42946a57a7a76037d1
gani3 <ganialabdul@gmail.com>
2022-02-25 09:24:24 +0700

    membuat koneksi ke database dan setting express

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 8080;
+
+
+
+// parse aplikasi yang berformat js ke json
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log(`Silahkan akses ke http://localhost:${port}`);
+});
+
+app.get('path', (req, res) => {
+
+});
---
+++ config/koneksi.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+// proses pembuatan authentikasi database
+const koneksi = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'belajar_nodejs_api',
+  database: 'belajar_nodejs_api'
+});
+
+// proses connce ke database
+koneksi.connect((err) => {
+  if (err) throw err;
+  console.log('koneksi berhasil ke mysql');
+});
+
+module.exports = {
+  koneksi
+}

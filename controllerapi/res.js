da88c44e56696792c641063a8c6c2376633c79fe
gani3 <ganialabdul@gmail.com>
2022-02-25 09:27:36 +0700

    konfigurasi agar js berjalan strict

---
+++ controllerapi/res.js
@@ -0,0 +1,11 @@
+'use strict'; //untuk mengkonversi semua kode agar sesuai ketentuan js
+
+// mengirim status apabila api berhasil dijalankan
+exports.ok = function (values, res) {
+  const data = {
+    'status': 200,
+    'values': values
+  };
+  res.json(data);
+  res.end();
+}

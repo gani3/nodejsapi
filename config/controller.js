987c78df4178862360d323fcba10ee24174085b4
gani3 <ganialabdul@gmail.com>
2022-02-25 10:12:15 +0700

    menampilkan data

--- config/controller.js
+++ config/controller.js
@@ -1,9 +1,39 @@
 'use strict';
 
 const respon = require('./res');
-const connect = require('./koneksi');
+const {
+  koneksi
+} = require('./koneksi');
 
 exports.index = function (req, res) {
   respon.ok("Aplikasi rest Api berhasil berjalan", res);
+
+}
+
+// membaca semua data
+exports.lihatsemuadata = function (req, res) {
+  koneksi.query('SELECT * FROM mahasiswa', function (error, rows, field) {
+    if (error) {
+      console.log(error);
+    } else {
+      respon.ok(rows, res);
+    }
+  });
+}
+
+// mengambil data berdasarkan id
+exports.detailData = function (req, res) {
+  let id = req.params.id;
+  koneksi.query(`SELECT * FROM mahasiswa WHERE idmahasiswa =  ${id}`,
+    function (error, rows, field) {
+      if (error) {
+        console.log(error);
+      } else {
+        respon.ok(rows, res);
+      }
+    });
+
+  // tambah data menggunakan API
+
 
 }
--- config/router.js
+++ config/router.js
@@ -2,6 +2,10 @@
 module.exports = function (app) {
   const json = require('./controller');
 
-  app.route('/')
-    .get(json.index);
+  app.route('/').get(json.index);
+
+  // menampilkan data dari controller
+  app.route('/tampil').get(json.lihatsemuadata);
+
+  app.route('/tampil/:id').get(json.detailData);
 }
